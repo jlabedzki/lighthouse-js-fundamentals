ff6905c1deacd2e5f76cd8f076cf868b6a35125e
Johnny <johnnylabedzki@hotmail.com>
2021-07-14 09:56:42 -0700

    Added a return statement to greeting function

--- hello.js
+++ hello.js
@@ -3,3 +3,12 @@ const sayHello = function (name) {
 }
 
 sayHello('Johnny');
+
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('Johnny');
+console.log(greeting);
+
